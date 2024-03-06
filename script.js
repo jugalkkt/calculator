e15ae1b35108a13c9d9307e282ee7f4b94c9965b
jugalkkt <jugalkakkat@gmail.com>
2024-03-06 13:42:41 +0530

    Declare variables for operator and operands

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ let add = (a, b) => (a + b);
 let multiply = (a, b) => (a * b);
 let subtract = (a, b) => (a - b);
 let divide = (a, b) => (a / b);
+let operand1 = prompt("Enter operand 1: ");
+let operand2 = prompt("Enter operand 2: ");
+let opeartor = prompt("Enter operator: ");
