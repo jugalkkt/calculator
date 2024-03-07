1d64cf4c7414d76801825c1d0f69ee19ad4cf578
jugalkkt <jugalkakkat@gmail.com>
2024-03-07 10:29:07 +0530

    Add operate function

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ let add = (a, b) => (a + b);
 let multiply = (a, b) => (a * b);
 let subtract = (a, b) => (a - b);
 let divide = (a, b) => (a / b);
-let operand1 = prompt("Enter operand 1: ");
-let operand2 = prompt("Enter operand 2: ");
-let opeartor = prompt("Enter operator: ");
+let operand1 = parseFloat(prompt("Enter operand 1: "));
+let operand2 = parseFloat(prompt("Enter operand 2: "));
+let operator = prompt("Enter operator: ");
+let operate = (op1, op2, operator) =>
+{
+    switch (operator)
+    {
+        case '+': 
+            return add(op1, op2);
+            break;
+        case '-': 
+            return subtract(op1, op2);
+            break;
+        case '*':
+            return multiply(op1, op2);
+            break;
+        case '/':
+            return divide(op1,op2);
+            break;
+    }
+}
