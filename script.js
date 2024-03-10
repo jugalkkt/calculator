4a74a7a8ebd931eb5d7c8ee5ed101f8b622ee20e
jugalkkt <jugalkakkat@gmail.com>
2024-03-10 23:55:54 +0530

    Seperate inputs to operator, prev, curr

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 let previousContent = '';
 let currentContent = '';
+let operator = '';
+
 let numbers = document.querySelectorAll('.number');
 let operators = document.querySelectorAll('.operator');
 
 let previousScreen = document.querySelector('.previous');
 let currentScreen = document.querySelector('.current');
 let clear = document.querySelector('.clear');
-function handleNum(num) {
+let decimal = document.querySelector('.decimal');
+let equal = document.querySelector('.equal');
+
+
+function handleNum(num) 
+{
     if (currentContent.length < 6) currentContent += num;
     currentScreen.textContent = currentContent;
 }
-function handleOp(op){
-    previousContent = currentContent + ' ' + op;
+function handleOp(op)
+{
+    operator = op;
+    previousContent = currentContent;
     currentContent = '';
-    previousScreen.textContent = previousContent;
+}
+function clearScreen()
+{
+    previousContent = '';
+    currentContent = '';
     currentScreen.textContent = currentContent;
+    previousScreen.textContent = previousContent;
 }
 
+
 document.addEventListener('DOMContentLoaded', () => {
-
-
     numbers.forEach((number) => number.addEventListener('click', function (e) {
         handleNum(e.target.textContent);
+        previousScreen.textContent = previousContent + ' ' + operator;
+        currentScreen.textContent = currentContent;
     }))
     operators.forEach((op) => op.addEventListener('click', function(e){
         handleOp(e.target.textContent);
+        previousScreen.textContent = previousContent + ' ' + operator;
     }))
+    // clear.addEventListener('click', clearScreen);
 })
